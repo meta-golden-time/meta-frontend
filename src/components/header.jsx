0fa4bb9f6ac37b8c89b3e7b972d4c4d9ac6a47bd
TwoZeroLibrary <50097872+TwoZeroLibrary@users.noreply.github.com>
2024-06-10 18:47:11 +0900

    update0610-3

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,16 @@
 import {Nav} from '@components/nav';
-import MenuBarHeader from './ui/menuBarHeader';
+import MenuBarHeader from './menuBarHeader';
 
 export default function Header() {
   return(
-    // <div style={{display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+    <>
     <div>
       <MenuBarHeader/>
     </div>
+    <hr />
+    {/* <div style={{display: 'flex', justifyContent: 'flex-end', width: '100%', height: '50px', verticalAlign: 'bottom'}}>
+      <p>유저 페이지</p>
+    </div> */}
+    </>
   )
 } 
--- src/components/menuBarHeader.jsx
+++ src/components/menuBarHeader.jsx
@@ -13,21 +13,21 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-// import AdbIcon from '@mui/icons-material/Adb';
 
 // "디바이스" 자체의 화면폭을 확인해서 true/false를 반환해주는 리액트 전용 훅
 import useMediaQuery from '@mui/material/useMediaQuery';
 // 테마 객체를 가져옴 : 어플리케이션의 색상, 폰트 크기, 브레이크포인트 등 다양한 스타일 속성을 포함하고 있음
 import { useTheme } from '@mui/material/styles';
-import myLogo from '../../img/main/goldenTimeLogo.png';
+// 이미지 가져오기
+import myLogo from '../img/main/goldenTimeLogo.png';
+import { useNavigate } from 'react-router-dom';
 
 // 페이지 메뉴 항목을 정의
-const pages = ['weather', 'map'];
+const pages = {weather: 'weather', map: 'map'};
+
 // 사용자 설정 메뉴 항목을 정의
-const settings = ['Profile', 'Account', 'Logout'];
+const settings = {'User Page': 'user/userPage', 'Logout': 'logout'};
 
-
-// 네비게이션 메뉴와 사용자 설정 메뉴의 항목들을 정의
 function MenuBarHeader() {
   // 내비게이션 메뉴의 열림 상태를 관리하는 상태 훅을 정의
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -43,6 +43,12 @@ function MenuBarHeader() {
     setAnchorElUser(event.currentTarget);
   };
 
+  // 페이지 이동 함수
+  const navigate = useNavigate();
+  const handleMovePage = (page) => {
+    navigate(`/${page}`)
+  };
+
   const handleCloseNavMenu = () => {
     // 내비게이션 메뉴를 닫는 함수
     setAnchorElNav(null);
@@ -54,20 +60,16 @@ function MenuBarHeader() {
   };
 
   const theme = useTheme();
-
   // 화면 크기가 'md' (기본적으로 960px) 이하일 때를 의미하는 미디어 쿼리 조건을 생성
   const isMobile = useMediaQuery(theme.breakpoints.down('md')); // 모바일 화면 여부 확인
   const isPc = useMediaQuery(theme.breakpoints.up('md')); // pc 화면 여부 확인
 
   return (
-    <AppBar class=".MuiAppBar-colorTransparent" position="static" style={{ width: '100%' }}> {/* AppBar 컴포넌트를 사용하여 상단 바를 생성합니다. */}
-      <Container maxWidth="xl"> {/* 최대 폭이 'xl'인 Container 컴포넌트를 사용합니다. */}
-        <Toolbar disableGutters> {/* Toolbar 컴포넌트를 사용하여 도구 모음을 생성합니다. disableGutters는 패딩을 제거합니다. */}
+    <AppBar class=".MuiAppBar-colorTransparent" position="static" style={{ width: '100%' }}> {/* AppBar 컴포넌트를 사용하여 상단 바를 생성 */}
+      <Container maxWidth="xl"> {/* 최대 폭이 'xl'인 Container 컴포넌트를 사용 */}
+        <Toolbar disableGutters> {/* Toolbar 컴포넌트를 사용하여 도구 모음을 생성합니다. disableGutters는 패딩을 제거 */}
 
-          {/* Adb 아이콘을 표시합니다. xs 크기에서는 숨기고 md 크기 이상에서는 표시합니다. */}
-          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />  */}
-          {/* 큰 화면(pc 화면)이 작은 화면(모바일 화면)으로 전환 될 때 아이콘 표시 여부를 나타내주는 AdbIcon 태그 코드를
-          아래 Box 태그 코드로 수정함 */}
+          {/* 큰 화면(pc 화면)이 작은 화면(모바일 화면)으로 전환 될 때 아이콘 표시 여부를 나타내줌 */}
           {!isMobile && (
             <Box component="img" src={myLogo} alt="My Logo" sx={{ width: { xs: 25, sm: 30, md: 33 }, height: { xs: 25, sm: 30, md: 33 }, mr: 0.5 }} />
           )}
@@ -76,7 +78,7 @@ function MenuBarHeader() {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
@@ -90,8 +92,8 @@ function MenuBarHeader() {
             Golden Time
           </Typography>
 
+          {/* 작은 화면에서는 내비게이션 메뉴를 위한 아이콘 버튼을 표시 */}
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            {/* 작은 화면에서는 내비게이션 메뉴를 위한 아이콘 버튼을 표시합니다. */}
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -100,7 +102,7 @@ function MenuBarHeader() {
               onClick={handleOpenNavMenu}
               color="inherit"
             >
-              <MenuIcon /> {/* 메뉴 아이콘을 표시합니다. */}
+              <MenuIcon /> {/* 메뉴 아이콘을 표시 */}
             </IconButton>
             <Menu
               id="menu-appbar"
@@ -120,28 +122,27 @@ function MenuBarHeader() {
                 display: { xs: 'block', md: 'none' },
               }}
             >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
+              {Object.keys(pages).map((page) => (
+                <MenuItem key={page} 
+                onClick={() => handleMovePage(pages[page])}
+                >
                   <Typography textAlign="center">{page}</Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
 
-          {/* 작은 화면에서는 Adb 아이콘을 표시합니다. */}
-          {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
-          {/* 작은 화면(모바일 화면)이 큰 화면(pc 화면)으로 전환 될 때 아이콘 표시 여부를 나타내주는 AdbIcon 태그 코드를
-          아래 Box 태그 코드로 수정함 */}
+          {/* 작은 화면(모바일 화면)이 큰 화면(pc 화면)으로 전환 될 때 아이콘 표시 여부를 나타내줌 */}
           {!isPc && (
             <Box component="img" src={myLogo} alt="My Logo" sx={{ width: { xs: 25, sm: 30, md: 33 }, height: { xs: 25, sm: 30, md: 33 }, mr: 1 }} />
           )}
 
-          {/* 작은 화면에서는 'LOGO' 텍스트를 표시합니다. */}
+          {/* 작은 화면에서는 'Golden Time' 텍스트를 표시 */}
           <Typography
             variant="h5"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 1,
               display: { xs: 'flex', md: 'none' },
@@ -156,25 +157,25 @@ function MenuBarHeader() {
             Golden Time
           </Typography>
 
+
+          {/* 큰 화면에서는 메뉴 버튼을 표시 */}
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
-            {/* 큰 화면에서는 내비게이션 메뉴 버튼을 표시합니다. */}
-            {pages.map((page) => (
+            {Object.keys(pages).map((page) => (
               <Button
                 key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'black', display: 'block' }}
+                onClick={() => handleMovePage(pages[page])}
+                sx={{ my: 1, color: 'black', display: 'block' }}
               >
                 {page}
               </Button>
             ))}
           </Box>
 
-          {/* 사용자 아이콘은 오른쪽에 유지 */}
-          <Box sx={{ flexGrow: 0 }}>
-            {/* 사용자 메뉴를 위한 아이콘 버튼을 표시합니다. */}
+          {/* 사용자 메뉴를 위한 아이콘 버튼을 표시 */}
+          <Box sx={{ flexGrow: 0, pl: 3 }}> {/* 사용자 아이콘은 오른쪽에 유지 */}
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> {/* 아바타 이미지를 표시합니다. */}
+                <Avatar alt="User Nick Name" src="/static/images/avatar/2.jpg" /> {/* 아바타 이미지를 표시 --> 로그인시 설정된 아이콘으로 이미지 표시할 것*/}
               </IconButton>
             </Tooltip>
             <Menu
@@ -193,8 +194,9 @@ function MenuBarHeader() {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+              {/* 아이콘 버튼 클릭 후 나오는 네비게이션 바 리스트에 대한 페이지 이동 버튼 */}
+              {Object.keys(settings).map((setting) => (
+                <MenuItem key={setting} onClick={() => handleMovePage(settings[setting])}>
                   <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
               ))}
