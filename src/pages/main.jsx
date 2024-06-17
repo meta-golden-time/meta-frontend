9f175b734d792f280be53b9f7c8216a302caeded
TwoZeroLibrary <50097872+TwoZeroLibrary@users.noreply.github.com>
2024-06-18 07:28:15 +0900

    update6018-메인화면리뉴얼

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -2,96 +2,116 @@
 // data-aos="zoom-out-right" -> 오른쪽으로 줌아웃 하는 모션 설정
 // data-aos="fade-up" -> 아래에서 위로 올라오는 모션
 
-import { useEffect } from 'react';
 
+import { useEffect } from 'react';
 // 스크롤 애니메이션 AOS 라이브러리 불러오기
 import Aos from 'aos';
 import 'aos/dist/aos.css'; // AOS 스타일시트
-import '@styles/main.scss' // 커스텀 스타일시트
-import image from '@img/main/map_page_img.png'; // 이미지 파일 가져오기
+// full page 라이브러리
+import { Fullpage, FullPageSections, FullpageSection, FullpageNavigation } from '@ap.cx/react-fullpage';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap 스타일시트
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Bootstrap JavaScript
+import Content from '@components/content.jsx';
+import image from '@img/main/map_page_img.png';
+import '@styles/main.scss';
 
-function Main() {
+const Main = () => {
+
   useEffect(() => {
-    Aos.init({ duration: 1100 }); // AOS 애니메이션 초기화 및 지속시간 설정
+    Aos.init({ duration: 1200 }); // AOS 애니메이션 초기화 및 지속시간 설정
   }, []);
 
   return (
-    <div className="app">
-      <header className="headerBack">
-        <div className="headerTitle" data-aos="fade-down">
-          <h1>당신의 바쁜 아침 출근 길을 <p>도와줄 첫 번째 비서</p></h1>
-        </div>
-      </header>
+    <>
+      <Fullpage>
 
-      <div className='contentBunch'>
-        <section className="content" data-aos="fade-up" data-aos-delay="100">
-          <div className='divWeather'>
-            <h2>맞춤형 오늘의 날씨 정보</h2>
-            <p>당신이 설정한 위치나, 현재 장소의 날씨 정보를 제공해요.</p>
-          </div>
-          <div className='divWeaterImg'>
-            <img src={image} alt="" className="mainImage" data-aos="zoom-out-right" />
-          </div>
-          <div className='divWeaterBut'>
-            <button className="mainButton" data-aos="fade-up">
-              클릭하세요
-            </button>
-          </div>
-        </section>
+        {/* 네비게이션 도트 추가 */}
+        <FullpageNavigation/>
 
-        <section className='content' data-aos="fade-up" data-aos-delay="100">
-          <div>
-            <h2>편리한 길 찾기 서비스</h2>
-            <p>
-              출발 위치와 도착 위치를 입력하면 이용할 지하철이나 버스 또는 자가용,
-              교통편에 따라 최적의 경로를 제공해요.
-            </p>
-          </div>
-          <div>
-            <img src={image} alt="" className="mainImage" data-aos="zoom-out-right" />
-          </div>
-          <div>
-            <button className="mainButton" data-aos="fade-up">
-              클릭하세요
-            </button>
-          </div>
-        </section>
-
-        <section className='content' data-aos="fade-up" data-aos-delay="100">
-          <div>
-            <h2>경로 즐겨찾기</h2>
-            <p>자주 이용하는 경로를 빠르게 확인해보세요.</p>
-          </div>
-          <div>
-            <img src={image} alt="" className="mainImage" data-aos="zoom-out-right" />
-          </div>
-          <div>
-            <button className="mainButton" data-aos="fade-up">
-              클릭하세요
-            </button>
+        <FullPageSections>
+          
+          {/* 제목 섹션 */}
+          <FullpageSection style={{ height: '100vh' }}>
+            <div className="section titleBackground">
+              <div className="titleDiv" data-aos="fade-down">
+                <h1 className="mainTitle" style={{ color: 'white' }}>
+                  당신의 바쁜 아침 출근 길을 <p className='mainTitleP'>도와줄 <span style={{ color: '#007bff' }}>첫 번째 비서</span></p>
+                </h1>
+                {/* 버튼과 제목 사이의 간격을 위한 스타일 */}
+                <div style={{ marginTop: '50px' }}>
+                  <button type="button" className="btn custom-btn same-size-btn">weather</button>
+                  <button type="button" className="btn custom-btn same-size-btn">map</button>
+                </div>
+              </div>
             </div>
-        </section>
+          </FullpageSection>
+          
+          {/* 첫 번째 페이지 */}
+          <FullpageSection style={{ height: '100vh' }}>
+            <div className="section container" data-aos="fade-up" data-aos-delay="600">
+              <div className="row">
+                <div className="col-md-6">
+                  {/* 텍스트 콘텐츠 */}
+                  <Content className="display-4" title="맞춤형 오늘의 날씨 정보" description="당신이 설정한 위치나, 현재 장소의 날씨 정보를 제공해요." />
+                </div>
+                <div className="col-md-6">
+                  {/* 이미지 콘텐츠 */}
+                  <img src={image} className="img-fluid" alt="Example" />
+                </div>
+              </div>
+            </div>
+          </FullpageSection>
 
-        <section className='content' data-aos="fade-up" data-aos-delay="100">
-          <div>
-            <h2>알림 설정</h2>
-            <p>
-              이제는 졸아도 안심하세요. 원하는 교통 수단으로 경로를 선택하면 도착 10분 전 알림 받을
-              수 있어요.
-            </p>
-          </div>
-          <div>
-            <img src={image} alt="" className="mainImage" data-aos="zoom-out-right" />
-          </div>
-          <div>
-            <button className="mainButton" data-aos="fade-up">
-              클릭하세요
-            </button>
-          </div>
-        </section>
+          {/* 두 번째 페이지 */}
+          <FullpageSection style={{ height: '100vh' }}>
+            <div className="section" data-aos="fade-up" data-aos-delay="600">
+              <Content title="편리한 길 찾기 서비스" description="출발 위치와 도착 위치를 입력하면 이용할 지하철이나 버스 또는 자가용, 교통편에 따라 최적의 경로를 제공해요." />
+            </div>
+          </FullpageSection>
 
-      </div>
-    </div>
+          {/* 세 번째 페이지 */}
+          <FullpageSection style={{ height: '100vh' }}>
+            <div className="section" data-aos="fade-up" data-aos-delay="600">
+              <Content title="경로 즐겨찾기" description="자주 이용하는 경로를 빠르게 확인해보세요." />
+            </div>
+          </FullpageSection>
+
+          {/* 네 번째 페이지 */}
+          <FullpageSection style={{ height: '100vh' }} >
+            <div className="section d-flex justify-content-center align-items-center">
+              {/* 캐러셀 추가 */}
+              <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner" data-aos="fade-up" data-aos-delay="600">
+                  <div className="carousel-item active" data-bs-interval="6000">
+                    <Content title="편리한 길 찾기" image={image} />
+                  </div>
+                  <div className="carousel-item" data-bs-interval="2000">
+                    <Content title="경로 즐겨찾기" image={image} />
+                  </div>
+                  <div className="carousel-item">
+                    <Content title="알림 설정" image={image} />
+                  </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+          </FullpageSection>
+
+        </FullPageSections>
+      </Fullpage>
+    </>
   );
 }
 
