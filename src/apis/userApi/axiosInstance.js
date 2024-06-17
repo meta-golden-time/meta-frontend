c5fd8c07e7d672a9c10e2b713c0e797451dabb87
Kim-BanSeok <101537989+Kim-BanSeok@users.noreply.github.com>
2024-06-17 19:39:37 +0900

    배포과정1

--- src/apis/userApi/axiosInstance.js
+++ src/apis/userApi/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000';
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
