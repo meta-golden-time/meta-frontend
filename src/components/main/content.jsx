c7c827592e85cb57d7a3c644304b4913024045fd
TwoZeroLibrary <50097872+TwoZeroLibrary@users.noreply.github.com>
2024-06-24 11:15:27 +0900

    update0624-메인페이지,메뉴바및버튼수정

--- src/components/main/content.jsx
+++ src/components/main/content.jsx
@@ -3,14 +3,9 @@ import '@styles/main/content.scss';
 const Content = ({ title, description, image }) => {
   return (
     <div>
-      <div>
-        <h1 className="subtitle">{title}</h1>
-        {/* props 값이 들어오면 내용이 보이고 들어오지 않으면 보이지 않음 */}
-        {description && <p className="content">{description}</p>}
-      </div>
-      <div className="content-img-div" >
-        {image && <img className="content-img" src={image} alt={title} />}
-      </div>
+      <h1 className="subtitle">{title}</h1>
+      {/* props 값이 들어오면 내용이 보이고 들어오지 않으면 보이지 않음 */}
+      {description && <p className="content">{description}</p>}
     </div>
   );
 }
